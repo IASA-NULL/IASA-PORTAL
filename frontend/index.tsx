a2bca3b4414748e8eb19f2ed79a63f96db599979
Seohyun Lee <04seohyun@gmail.com>
2021-02-20 11:34:10 +0900

    Fixed logo animation bug

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -406,6 +406,31 @@ class App extends React.Component<any, IState> {
     }
 }
 
+function init() {
+    try {
+        document.getElementById('preloader').classList.remove('anim')
+    } catch (e) {}
+    //@ts-ignore
+    window.scrollObj = OverlayScrollbars(document.body, {
+        callbacks: {
+            onScroll: (e: any) => {
+                if (e.target.scrollTop === 0)
+                    document.querySelector('header').classList.remove('raised')
+                else document.querySelector('header').classList.add('raised')
+            },
+        },
+    })
+    ReactDOM.render(<App />, document.getElementById('app') as HTMLElement)
+    setTimeout(function () {
+        document.getElementById('preloader').style.opacity = '0'
+        setTimeout(function () {
+            document.getElementById('preloader').remove()
+        }, 500)
+    }, 100)
+}
+
+declare const timerStart: number
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log(
         '%cIASA Portal\n%cCopyright 2019-2021 NULL®. All right reserved.\n%c경고!\n%c이곳에서 뭔가를 복사하거나 수정할 때는 꼭 무엇을 의미하는지 알아야 합니다. 신뢰할 수 없는 행위를 하는 경우 계정 해킹, 도용, 삭제 등의 심각한 상황이 일어날 수 있습니다. NULL은 이 문제에 관해 책임을 지지 않습니다.',
@@ -414,7 +439,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'font-size:30px;color:red;',
         'font-size:15px;color:red;'
     )
-    ReactDOM.render(<App />, document.getElementById('app') as HTMLElement)
+    setTimeout(init, Math.max(2000 - Date.now() + timerStart, 0))
 })
 
 export default App
