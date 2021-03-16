9cef6d5f8c5554d51be24fb0afddb2b0f7d92523
Seo-Rii <StudioSeoRii@gmail.com>
2021-03-16 11:46:34 +0900

    fix: release ip v5.0.2

--- backend/api/ip.ts
+++ backend/api/ip.ts
@@ -3,12 +3,12 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/ver', async (req, res) => {
-    res.send('501')
+    res.send('502')
 })
 
 router.get('/link/lastest', async (req, res) => {
     res.send(
-        'https://s3.ap-northeast-2.amazonaws.com/public.iasa.kr/IP/bin/IP_5_0_1.exe'
+        'https://s3.ap-northeast-2.amazonaws.com/public.iasa.kr/IP/bin/IP_5_0_2.exe'
     )
 })
 
