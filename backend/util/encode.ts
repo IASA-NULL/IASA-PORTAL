b74d8c6d80d1cf149209d3bc0a660a8753ca3833
Seo-Rii <StudioSeoRii@gmail.com>
2021-03-21 16:50:33 +0900

    fix: fixed Korean encoding error

--- backend/util/encode.ts
+++ backend/util/encode.ts
@@ -3,24 +3,8 @@ import iconvLite from 'iconv-lite'
 export function getDownloadFilename(filename: string, req: any) {
     const header = req.headers['user-agent']
 
-    if (header.includes('MSIE') || header.includes('Trident')) {
+    if (header.includes('MSIE') || header.includes('Trident'))
         return encodeURIComponent(filename).replace(/\\+/gi, '%20')
-    } else if (header.includes('Chrome')) {
-        return iconvLite.decode(
-            iconvLite.encode(filename, 'UTF-8'),
-            'ISO-8859-1'
-        )
-    } else if (header.includes('Opera')) {
-        return iconvLite.decode(
-            iconvLite.encode(filename, 'UTF-8'),
-            'ISO-8859-1'
-        )
-    } else if (header.includes('Firefox')) {
-        return iconvLite.decode(
-            iconvLite.encode(filename, 'UTF-8'),
-            'ISO-8859-1'
-        )
-    }
 
-    return filename
+    return iconvLite.decode(iconvLite.encode(filename, 'UTF-8'), 'ISO-8859-1')
 }
